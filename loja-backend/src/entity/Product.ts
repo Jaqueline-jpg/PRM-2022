58f0274d9a3577393a7e58a1957a64c6fc102e76
Jaqueline-jpg <jaquelineribeeiroo@gmail.com>
2022-08-09 21:41:36 -0300

    Aula 09/08/2022

---
+++ loja-backend/src/entity/Product.ts
@@ -0,0 +1,35 @@
+import { Column, CreateDateColumn, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Category } from "./Category";
+import { Brand} from "./Brand";
+
+
+@Entity()
+export class Product {
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column( {nullable: false, length: 50})
+    name: string;
+
+    @Column('text', {nullable: true})
+    description: string;
+
+    @Column({nullable: false})
+    price: number;
+
+    @Column( {nullable: false, length: 1})
+    active: string;
+
+    @ManyToOne(() => Category, {eager: true, nullable: false} )
+    category: Category;
+
+    @ManyToOne(() => Brand, {eager: true, nullable: true} )
+    brand: Brand;
+
+    @CreateDateColumn()
+    createdAT: Date;
+
+    @UpdateDateColumn()
+    updatedAT: Date;
+}
--- loja-backend/src/server.ts
+++ loja-backend/src/server.ts
@@ -1,4 +1,6 @@
+import { AppDataSource } from './data-source';
 import express from 'express';
+import cors from 'cors';
 
 //Instâncio uma aplicação express
 const app = express();
@@ -6,6 +8,18 @@ const app = express();
 //Determina a porta de execução
 const PORT = 3300;
 
-app.listen(PORT, () => {
-    console.log(`Running in port ${PORT}`);
-})
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Se conectar no banco de dados, levanto a aplicação
+AppDataSource.initialize().then(() => {
+
+    //Levanto a aplicação
+    app.listen(PORT, () => {
+        console.log(`Server running in port ${PORT}`);
+    })
+
+}).catch(error => {
+    console.log('Ops, não conectei no banco de dados', error);
+});
